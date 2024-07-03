25f49d0d2e1b3060285a996079c8808069c4006b
boomchanotai <boom9387@hotmail.com>
2024-07-03 12:41:40 +0700

    fix: container size

--- frontend/src/lib/components/Container.svelte
+++ frontend/src/lib/components/Container.svelte
@@ -2,6 +2,6 @@
 	import { cn } from '@/utils';
 </script>
 
-<div class={cn('max-w-7xl mx-auto px-6 sm:px-6 lg:px-8 ', $$props.class)}>
+<div class={cn('max-w-lg mx-auto px-6 sm:px-6 lg:px-8 ', $$props.class)}>
 	<slot />
 </div>
