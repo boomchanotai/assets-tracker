76860cd309c2881cd3cdef6adab62f8cdbebcfd8
boomchanotai <boom9387@hotmail.com>
2024-07-03 11:07:09 +0700

    feat: add update balance modal

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,17 +4,33 @@
 	import Container from '@/components/Container.svelte';
 	import Pocket from '@/components/Pocket.svelte';
 	import Button from '@/components/ui/button/button.svelte';
+	import * as Dialog from '$lib/components/ui/dialog';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
+	import Balance from '@/components/Balance.svelte';
 </script>
 
 <div class="space-y-4">
 	<Header />
 
 	<Container class="flex flex-row justify-between">
-		<div>
-			<h2 class="text-xs">Total</h2>
-			<p class="font-semibold text-xl">$ 13500 THB</p>
-		</div>
-		<Button>อัพเดตยอดเงิน</Button>
+		<Balance amount={14500} />
+		<Dialog.Root>
+			<Dialog.Trigger>
+				<Button>อัพเดตยอดเงิน</Button>
+			</Dialog.Trigger>
+			<Dialog.Content class="sm:max-w-[425px]">
+				<Dialog.Header>
+					<Dialog.Title>Update Balance</Dialog.Title>
+				</Dialog.Header>
+				<div>
+					<Input id="balance" type="number" placeholder="Balance" />
+				</div>
+				<Dialog.Footer>
+					<Button type="submit">Save changes</Button>
+				</Dialog.Footer>
+			</Dialog.Content>
+		</Dialog.Root>
 	</Container>
 
 	<Container class="space-y-8">
