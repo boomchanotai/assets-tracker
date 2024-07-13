d4b591fed4bc296fbc6d162bd7a8897b1cbfedc2
boomchanotai <boom9387@hotmail.com>
2024-07-14 02:49:22 +0700

    fix: trash

--- apps/web/src/lib/components/Trash.svelte
+++ apps/web/src/lib/components/Trash.svelte
@@ -2,13 +2,16 @@
 	import { cn } from '@/utils';
 	import Icon from '@iconify/svelte';
 	import Draggable from './Draggable.svelte';
+	import Pocket from './Pocket.svelte';
 
+	export let pockets: Pocket[];
 	const id = 'trash';
 </script>
 
 <Draggable
 	draggableId={id}
 	draggable={false}
+	{pockets}
 	class={cn(
 		'w-full h-full flex flex-col justify-center items-center border border-black rounded-lg p-4 [&_*]:pointer-events-none select-none',
 		$$props.class
